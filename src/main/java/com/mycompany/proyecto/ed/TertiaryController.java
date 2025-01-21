e957340c6de415d2249200763aaaf5c179777bca
Johao Dorado <jcdorado@espol.edu.ec>
2025-01-20 23:39:25 -0500

    cambio name

--- src/main/java/com/mycompany/proyecto/ed/TertiaryController.java
+++ src/main/java/com/mycompany/proyecto/ed/TertiaryController.java
@@ -5,12 +5,9 @@
 package com.mycompany.proyecto.ed;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Objects;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
